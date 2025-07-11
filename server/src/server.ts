32bc6b5765cd47e893f140f5107fbcad0e3ecbb3
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2025-07-10 21:36:12 -0300

    feat: adding new routes to create and list rooms and questions

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,9 @@ import {
 import { fastifyCors } from '@fastify/cors';
 import { env } from './env.ts';
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+import { createRoomRoute } from "./http/routes/create-room.ts";
+import { getRoomQuestions } from "./http/routes/get-room-questions.ts";
+import { createQuestionRoute } from "./http/routes/create-question.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -18,6 +21,9 @@ app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(getRoomQuestions);
+app.register(createQuestionRoute);
 
 app.listen({ port: env.PORT }).then(() => {
     console.log('HTTP Server running!')
