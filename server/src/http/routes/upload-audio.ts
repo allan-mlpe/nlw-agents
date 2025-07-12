399cc711e0dadc1aba2102af7b49951a172b098b
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2025-07-12 18:37:11 -0300

    feat: transcribing the audio received from the frontend

--- server/src/http/routes/upload-audio.ts
+++ server/src/http/routes/upload-audio.ts
@@ -2,6 +2,7 @@ import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { db } from '../../db/connection.ts';
 import { schema } from '../../db/schema/index.ts';
 import { z } from 'zod/v4';
+import { transcribeAudio } from "../../services/gemini.ts";
 
 export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
     app.post('/rooms/:roomId/audio', {
@@ -12,10 +13,17 @@ export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
         },
     }, async (request, reply) => {
         const { roomId } = request.params;
-        const audio = await request.file;
+        const audio = await request.file();
 
         if (!audio) {
             throw new Error('Audio is required.');
         }
+
+        const audioBuffer = await audio.toBuffer(); // espera o arquivo ser carregado por completo, uma vez que serão arquivos pequenos
+        const audioAsBase64 = audioBuffer.toString('base64');
+
+        const transcription = await transcribeAudio(audioAsBase64, audio.mimetype);
+
+        return { transcription };
     });
 };
