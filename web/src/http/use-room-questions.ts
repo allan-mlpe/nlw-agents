b39d4d547fe2599f8b952b5ef6046faf97b53660
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2025-07-11 21:02:46 -0300

    feat: adding the question-list component

--- web/src/http/use-room-questions.ts
+++ web/src/http/use-room-questions.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import type { GetRoomQuestionsResponse } from './types/get-room-questions-response';
 
-export function useRooms(roomId: string) {
+export function useRoomQuestions(roomId: string) {
     return useQuery({
         queryKey: ['get-questions', roomId],
         queryFn: async () => {
